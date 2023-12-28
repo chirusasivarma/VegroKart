0a47a67e3ea95ed5efce6f926b866d44a9f3e25d
umeshams28 <140066238+chiru2721@users.noreply.github.com>
2023-12-28 16:11:00 +0530

    commit

---
+++ VegroKart/src/main/java/com/example/VegroKart/Entity/Status.java
@@ -0,0 +1,10 @@
+package com.example.VegroKart.Entity;
+
+public class Status {
+
+	public final static String Delivered = "Delivered";
+	
+	public final static String Cancelled="Cancelled";
+	
+	public final static String WAITING="On the way";
+}
